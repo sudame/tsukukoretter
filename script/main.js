35c5f76de43dbb89be436c55b883c9319cdce485
Suda Mikihiro <s1711373@s.tsukuba.ac.jp>
2017-08-25 22:15:24 +0900

    add menu column

---
+++ script/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $("#men-switch").on('click', function(){
+        $(".men-column").toggle();
+    });
+
+    $("#women-switch").on('click', function(){
+        $(".women-column").toggle();
+    });
+})
+
